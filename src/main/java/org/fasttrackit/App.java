8407699434a90364ff1727ade2fdf0bb6e608dbd
Raul <ratiugabrielraul@gmail.com>
2019-06-11 20:13:44 +0300

    made classes and I give them properties

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,60 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args ) {
+
+        Animal animal = new Animal();
+        animal.name = "Bella";
+        animal.age = 3;
+        animal.hunger = 5;
+        animal.favoriteFood = "pedigree";
+        animal.favoriteActivity = "running";
+        animal.healthStatus = 6;
+        animal.mood = 5;
+
+
+
+        Adopter adopter = new Adopter();
+        adopter.name = "Cristian";
+        adopter.money = 20.33;
+
+
+        Vet vet = new Vet();
+        vet.name = "Alex";
+        vet.price = 10;
+        vet.specialization = "doctor";
+
+        Food food = new Food();
+        food.name = "pedigree";
+        food.expirationDate = LocalDateTime.now();
+        food.price = 5.67;
+        food.stock = true;
+        food.capacity = 844.123;
+
+
+
+
+        RecreationActivity activity = new RecreationActivity();
+        activity.name = "running";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
